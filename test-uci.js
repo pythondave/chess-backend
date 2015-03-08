f6b9af906374e314b66741435f6175b57826777f
Ryan Randall <pythondave@gmail.com>
2015-03-08 22:01:18 +0000

    demo of uci chess engine interaction using node package 'uci'

---
+++ test-uci.js
@@ -0,0 +1,41 @@
+/*
+To see in action:
+
+1. cmd
+2. cd C:\inetpub\wwwroot\chess-backend
+3. node test-uci.js
+*/
+
+var Engine = require('uci');
+var engine = new Engine('C:\\Program Files (x86)\\Arena\\Engines\\Stockfish\\stockfish_14053109_x64.exe');
+engine.runProcess().then(function () {
+    console.log('Started');
+    return engine.uciCommand();
+}).then(function (idAndOptions) {
+    console.log('Engine name - ' + idAndOptions.id.name);
+    return engine.isReadyCommand();
+}).then(function () {
+    console.log('Ready');
+    return engine.uciNewGameCommand();
+}).then(function () {
+    console.log('New game started');
+    return engine.positionCommand('startpos', 'e2e4 e7e5');
+}).then(function () {
+    console.log('Starting position set');
+    console.log('Starting analysis');
+    return engine.goInfiniteCommand(function infoHandler(info) {
+        console.log(info);
+    });
+}).delay(2000).then(function () {
+    console.log('Stopping analysis');
+    return engine.stopCommand();
+}).then(function (bestmove) {
+    console.log('Bestmove: ');
+    console.log(bestmove);
+    return engine.quitCommand();
+}).then(function () {
+    console.log('Stopped');
+}).fail(function (error) {
+    console.log(error);
+    process.exit();
+}).done();
